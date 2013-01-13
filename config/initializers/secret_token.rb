5622ad2ebc181c4cca034f34fc1fdc81c8c87fcf
Stephen Creedon <steve@creedon.me>
2013-01-13 21:38:38 +0000

    added twitter bootstrap and theme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RentifyAdmin::Application.config.secret_token = 'c2cb0617d99f5d6b1f8f36853e1216d38189d6ea6c2c8f6d2d239643ccca6a866fc0eb1e0c14d31dd8ec87d266cad4688fb5a1f7319fed421e034c6f8d74181c'
