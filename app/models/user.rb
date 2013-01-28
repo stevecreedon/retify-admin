8e3159794cdf97cfeccf0f4a16553885aec4ede6
Stephen Creedon <steve@creedon.me>
2013-01-28 09:07:01 +0000

    bit more commening around users and oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   has_many :identities
 
   def self.from_omniauth(auth)
+    #note we don't use name in this lookup. Suspect uid is a combination of oauth[name] and password 
     identity = Identity.where(provider: auth["provider"], password_digest: auth["uid"]).first
     if identity
       return identity.user
@@ -20,8 +21,8 @@ class User < ActiveRecord::Base
       identity = Identity.new
       identity.provider = auth["provider"]
       identity.password_digest = auth["uid"]
-      identity.name = auth["info"]["name"]
-      identity.email = auth["info"]["email"]#email not required in oauth spec
+      identity.name = auth["info"]["name"] #this can be email for some providers like omniauth-password
+      identity.email = auth["info"]["email"] #email not required in oauth spec. Twitter, for example, doesn't provide email
       user.identities << identity
     end
   end
